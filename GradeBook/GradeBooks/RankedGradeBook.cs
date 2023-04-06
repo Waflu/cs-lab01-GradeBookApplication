c21f5e45f3719234f85d7d79619be918c6e91386
Artur G <aglodek99@gmail.com>
2023-04-06 12:54:45 +0200

    CalculateStatistics CalculateStudentsStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -23,5 +23,23 @@ namespace GradeBook.GradeBooks
             }
             catch (Exception ex) { throw new InvalidOperationException(); } 
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine($"Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine($"Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
