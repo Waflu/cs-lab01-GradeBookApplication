7db392f3d8e4e007479e0ef0ce7b6f8541daed6b
Artur G <aglodek99@gmail.com>
2023-04-06 13:19:00 +0200

    GetGrade

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,20 +109,27 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+
+            double grade = 0;
+            if (studentType == StudentType.Honors|| studentType == StudentType.DualEnrolled)
+            {
+                grade += 1;
+            }
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + grade;
                 case 'B':
-                    return 3;
+                    return 3 + grade;
                 case 'C':
-                    return 2;
+                    return 2 + grade;
                 case 'D':
-                    return 1;
+                    return 1 + grade;
                 case 'F':
-                    return 0;
+                    return 0 + grade;
             }
-            return 0;
+            return grade;
         }
 
         public virtual void CalculateStatistics()
